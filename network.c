ef1660bf2a60cd2e56164897f7bea5763483ffde
bigbigstone <68249504@qq.com>
2020-12-22 03:52:36 -0800

    add branch

---
+++ network.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+int main()
+{
+    int lfd;
+    struct socketaddr_in ;
+    lfd = socket()
+}
